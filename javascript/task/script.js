b5f68b8053870cfab73b9f5008729ce2a25fff3c
Meer Quais <meerquais017@gmail.com>
2025-08-29 12:21:17 +0500

    task, if else and else if

---
+++ javascript/task/script.js
@@ -0,0 +1,86 @@
+/// Calculator App ///
+
+let score = 80;
+
+if(score >= 80){
+    alert("A+")
+}else if(score >= 70){
+    alert("A")
+}else if(score >= 60){
+    alert("B")
+}else if(score >= 50){
+    alert("C")
+}else if(score >= 40){
+    alert("D")
+}else if(score >= 33){
+    alert("E")
+}else{
+    alert("F")
+}
+
+alert("END OF CONDITION")
+
+// let num1 = prompt("Enter First Number", "1");   // String "3"
+// let num2 = prompt("Enter Second Number", "1"); // String  "3"
+// let operator = prompt("Select an Operator \n Add, Minus, Divide or Multiply", "Add") // "Add"
+
+// let convertedNum1 = Number(num1);
+// let convertedNum2 = Number(num2);
+
+// if(operator === "Add"){
+//     alert(convertedNum1 + convertedNum2)
+// }else if(operator === "Minus"){
+//     alert(convertedNum1 - convertedNum2)
+// }else if(operator == "Divide"){
+//     alert(convertedNum1 / convertedNum2)
+// }else{
+//     alert(convertedNum1 * convertedNum2)
+// }
+// if(operator === "Minus"){
+//     alert(convertedNum1 - convertedNum2)
+// }
+// if(operator == "Divide"){
+//     alert(convertedNum1 / convertedNum2)
+// }
+// if(operator == "Multiply"){
+//     alert(convertedNum1 * convertedNum2)
+// }
+
+/// QUIZ APP ///
+
+// let age = 18;
+
+// if(age >= 18){
+//     alert("You Are Adult")
+// }else{
+//     alert("You Are Not Adult")
+// }
+
+
+// alert("Total Quiz: 5 \n Passing Marks: 3 \n Rule: Enter Only Option Name eg. a, b, c or d")
+
+// let score = 0;
+
+// /// Question Number 1 ///
+
+// let ans1 = prompt("What does HTML stand for? \n a)Hyperlinks and Text Markup Language \n b)Hyper Text Markup Language \n c)Home Tool Markup Language")
+
+// if(ans1 == "b"){
+//     alert("Correct Answer");
+//     score++
+// }else{
+//     alert("Wrong Answer")
+// }
+
+// /// Question Number 2 ///
+
+// let ans2 = prompt("Choose the correct HTML element for the largest heading? \n a)<h6> \n b)<h1> \n c)<heading> \n d) <head>")
+
+// if(ans2 == "b"){
+//     alert("Correct Answer");
+//     score++
+// }else{
+//     alert("Wrong Answer")
+// }
+
+// alert("Your Score is " + score)
