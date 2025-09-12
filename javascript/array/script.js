d1d71f04fa65e93573c5656055fab7b0f1151c32
Meer Quais <meerquais017@gmail.com>
2025-09-12 12:36:12 +0500

    array

---
+++ javascript/array/script.js
@@ -0,0 +1,48 @@
+let str = "String"; // String
+let num = 123;     //  Number
+let bool = true;  //   Boolean
+
+// let city0 = "Karachi";
+// let city1 = "Lahore";
+// let city2 = "Islamabad";
+// let city3 = "Quetta";
+// let city4 = "Rawalpindi";
+// let city5 = "Peshawar";
+
+// city3 = "Ziyarat"
+
+let cities = ["Karachi", "Lahore", "Quetta", "Islamabad", "Rawalpindi", "Peshawar"] // Array
+
+        // Starting index, number of remove, add item/items
+// cities.splice(0, 0, "Hydrabad", "Gwadar")
+
+// cities[1]
+
+let newArr = cities.slice(0,3)
+
+// let newArr2 = [cities[0], cities[3], cities[5]]
+
+// cities.shift(); /// shift remove item from beginning of an array;
+// cities.unshift("Ziyarat", "Hydrabad") /// unshift add item/items on beginning of and array
+// cities.pop();
+// cities.pop();
+// cities[5] = false;
+// cities.pop();      /// pop item remove from end of an array
+// cities.push("Ziyarat", "Hydrabad") /// push add item/items on end of and array
+
+// cities[3] = "Ziyarat"
+
+// let cities = [];
+
+// cities[0] = "Karachi";
+// cities[1] = "Lahore";
+// cities[2] = "Islamabad";
+// cities[3] = "Quetta";
+
+// cities = ["Karachi",  ,  , "Quetta"]
+
+// alert(cities[2]);
+
+console.log(cities);
+
+console.log("newArr", newArr)
