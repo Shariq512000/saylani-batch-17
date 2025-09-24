73a59547b271417dc3c39125d96deeac438c14c7
Meer Quais <meerquais017@gmail.com>
2025-09-24 12:59:21 +0500

    some string methods

---
+++ javascript/loop-revision/script.js
@@ -0,0 +1,12 @@
+for(let i=1; i <= 10; i++){
+    console.log(i)
+    // line 1
+    // line 1
+    // line 1
+    // line 1
+    // line 1
+    // line 1
+    // line 1
+    // line 1
+    // line 10
+}
--- javascript/nested-loop-assignments/script.js
+++ javascript/nested-loop-assignments/script.js
@@ -7,14 +7,48 @@
 
 // i = 3 , j = 3
 
+// var          can redeclear and reassign
+// let          cannot redeclear but can reassign
+// const        cannot redeclear and reassign
 
-let str = "It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his.";
+// var newVar = "";
+// var newVar = "";
+// newVar = "";
+
+// const pi = 3.142;
+// const pi = "";
+
+// let newVar = "Declear";
+// newVar = "Reassign"
+
+
+let str = "It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available, World War II only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just, World War II enough like them to grasp their minds and motives in ways that they cannot grasp his.";
+
+// str = str.replaceAll("World War II", "the Second World War")
+
+// let firstIndex = str.indexOf("World War II")
+
+// if(firstIndex != -1){
+//     str = str.slice(0, firstIndex) + "the Second World War" + str.slice(firstIndex+12)
+// }
+
+// console.log(str)
+
+let j = 0;
 
 for(let i=0; i < str.length; i++){
     if(str.slice(i , i+12) == "World War II"){
-        
+        j++
+        if(j == 3){
+            str = str.slice(0, i) + "the Second World War" + str.slice(i+12)
+        }
+        // console.log(i)
     }
 }
+
+console.log(j)
+console.log(str)
+
 
 // let arr = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
