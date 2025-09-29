5a99dd7de1f275d69d75a6a035b6dd073e8230b3
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-09-29 12:54:33 +0500

    date and time

---
+++ javascript/date-time/script.js
@@ -0,0 +1,38 @@
+let rightNow = new Date();
+// let theDay = rightNow.getDay();
+// let theMonth = rightNow.getMonth();
+// let theDate = rightNow.getDate();
+// let theYear = rightNow.getFullYear();
+// let theHour = rightNow.getHours();
+// let theMinute = rightNow.getMinutes();
+// let theSecond = rightNow.getSeconds();
+// let theMilisecond = rightNow.getMilliseconds()
+// let months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "Octuber", "November", "December"]
+// let weeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+console.log(rightNow.getTime())
+
+// console.log(theHour);
+// console.log(theMinute);
+// console.log(theSecond)
+// console.log(theMilisecond)
+// console.log(theDate)
+// console.log(weeks[theDay])
+// console.log(months[theMonth])
+// console.log(theYear)
+
+// let hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+// let theHour = 15;
+
+// if(theHour == 0){
+//     console.log(12 + "am")
+// }else if(theHour > 12){
+//     console.log((theHour - 12) + "pm")
+// }else{
+//     console.log(theHour + "am")
+// }
+
+// if(theHour == 12){
+//     console.log(theHour / 2)
+// }
