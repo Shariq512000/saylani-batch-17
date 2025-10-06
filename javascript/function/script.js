12075b6a663cfc0f857eaa5717c540819154fee5
muhammdminhaj786 <eb20103087.mihajwahid@gmail.com>
2025-10-06 13:01:06 +0500

    function

---
+++ javascript/function/script.js
@@ -0,0 +1,22 @@
+// function timeCheck(){
+//     let newTime = new Date();
+//     let currentHours = newTime.getHours();
+//     let currentMinutes = newTime.getMinutes();
+//     alert(`${currentHours} : ${currentMinutes}`)
+// }
+
+// ////            parameter
+// function greeting(user){
+//     alert(`Hello ${user}!`)
+// }
+
+// ///      Argument
+// greeting("Shariq");
+
+
+function addition(value1 = 1, value2 = 0){
+    // alert(value1 + value2)
+    console.log(value1, value2);
+}
+
+addition()
