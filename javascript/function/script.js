b6aa8795274c873f79fb0416c062ad3d24066523
Meer Quais <meerquais017@gmail.com>
2025-10-08 13:01:54 +0500

    function

--- javascript/function/script.js
+++ javascript/function/script.js
@@ -5,7 +5,7 @@
 //     alert(`${currentHours} : ${currentMinutes}`)
 // }
 
-// ////            parameter
+////            parameter
 // function greeting(user){
 //     alert(`Hello ${user}!`)
 // }
@@ -13,10 +13,62 @@
 // ///      Argument
 // greeting("Shariq");
 
+///////////////////  Default Parameter
+// function addition(value1 = 0, value2 = 0){
+//     // console.log(value1 + value2);
+//     let result = value1 + value2
+//     return result
+// }
 
-function addition(value1 = 1, value2 = 0){
-    // alert(value1 + value2)
-    console.log(value1, value2);
+// function getTax(salary){
+//     return salary * 0.3
+// }
+
+// let person1Name = "Abc";
+// let person1 = getTax(1000)
+// let person2Name = "Def";
+// let person2 = getTax(3000)
+
+// console.log("person1", person1)
+// console.log("person2", person2)
+
+// shipment=5
+//Order 50 > 3 percent of remaining amout
+//Order 100>= free shipment
+
+function getTotalAmount(userOrder, voucherAmout = 0){
+    let grandTotal;
+    if(userOrder <= 50){
+        grandTotal = userOrder + 5;
+    }else if(userOrder >= 100){
+        grandTotal = ((userOrder - 50) * 0.03) + userOrder
+    }else{
+        grandTotal = ((userOrder - 50) * 0.03) + 5 + userOrder
+    }
+    return grandTotal - voucherAmout;
 }
 
-addition()
+// let order1 = [5, 4.6, 7.8, 16, 82, 5]
+
+
+function getPriceOfOrder(orders){
+    let orderTotal = 0;
+    
+    for(let i = 0; i < orders.length; i++){
+        orderTotal += orders[i]
+    }
+
+    return orderTotal
+}
+
+// console.log("orderTotal", orderTotal)
+
+let totalOrder1 = getPriceOfOrder([5, 4.6, 7.8, 16, 82, 5])
+
+let order1 = getTotalAmount(totalOrder1)
+let order2 = getTotalAmount(156);
+let order3 = getTotalAmount(40);
+
+console.log(`${order1}$`)
+console.log(`${order2}$`)
+console.log(`${order3}$`)
---
+++ javascript/global-and-local-variable/script.js
@@ -0,0 +1,32 @@
+// let newVar = 20;
+
+// if(true){
+//     let ifVar = "IF Test"
+//     if(true){
+//         let nestedVar = "Nested Test"
+//         // console.log(ifVar)
+//     }
+//     console.log(nestedVar)
+// }
+const var2 = 120;
+// var2 = 150
+// let var1 = "abc"
+// var1 = "def"
+// console.log("IF", ifVar)
+
+// var var3 = "asdasd"
+// var var3
+
+// function test(){
+//     let funcVar = "test"
+//     // console.log(newVar)
+//     if(true){
+//         let ifVar = "IF Test"
+//         console.log(funcVar)
+//     }
+//     console.log(ifVar)
+// }
+
+// test();
+
+// console.log(funcVar)
